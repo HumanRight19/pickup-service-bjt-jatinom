a193163017a7ffa9525d59cb651c48e039a7a503
HumanRight19 <dutaandra19@gmail.com>
2025-09-24 07:43:51 +0700

    perbaikan forgot password.vue

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,29 +1,28 @@
-<!--
-<script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+<!-- <script setup>
+// import GuestLayout from '@/Layouts/GuestLayout.vue';
+// import InputError from '@/Components/InputError.vue';
+// import InputLabel from '@/Components/InputLabel.vue';
+// import PrimaryButton from '@/Components/PrimaryButton.vue';
+// import TextInput from '@/Components/TextInput.vue';
+// import { Head, useForm } from '@inertiajs/vue3';
 
-defineProps({
-    status: {
-        type: String,
-    },
-});
+// defineProps({
+//     status: {
+//         type: String,
+//     },
+// });
 
-const form = useForm({
-    email: '',
-});
+// const form = useForm({
+//     email: '',
+// });
 
-const submit = () => {
-    form.post(route('password.email'));
-};
-</script>
+// const submit = () => {
+//     form.post(route('password.email'));
+// };
+</script> -->
 
 <template>
-    <GuestLayout>
+    <!-- <GuestLayout>
         <Head title="Forgot Password" />
 
         <div class="mb-4 text-sm text-gray-600">
@@ -65,6 +64,5 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
+    </GuestLayout> -->
 </template>
--->
